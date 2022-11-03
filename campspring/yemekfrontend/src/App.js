7542c141f99663e08007f72774457fbcc27df84d
Tahir Türker <turkertahir@gmail.com>
2022-11-03 17:05:31 +0300

    Component eklendi

--- campspring/yemekfrontend/src/App.js
+++ campspring/yemekfrontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Appbar from './components/Appbar';
 import Home from "./Pages/HomePage"
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <div className="App">
       <Appbar/>
+      <Dashboard/>
     </div>
   );
 }
---
+++ campspring/yemekfrontend/src/Pages/AlergenSelect.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Appbar from './components/Appbar'
+
+function AlergenSelect(){
+
+    return (
+        <div className="Home">
+            <Appbar/>
+        </div>
+    )
+
+}
